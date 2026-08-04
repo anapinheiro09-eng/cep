a3d85e06253371521af21874a7a2acaaee658e16
anapinheiro09-eng <ana.pinheiro09@escola.pr.gov.br>
2026-08-04 11:42:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputCep = document.querySelector('#cep');
+const btnbuscar = document.querySelector('#buscar');
+const divResultado =document.querySelector('#resultado');
+
+btnbuscar.addEventListener("click", buscarCEP);
+
+async function buscarCEP () {
+    const cepDigitado =inputCep.Value;
+
+    // (próximos passos)
+}
